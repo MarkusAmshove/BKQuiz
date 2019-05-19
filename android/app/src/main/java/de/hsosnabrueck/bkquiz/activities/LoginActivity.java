644e050bc717d5cd1a004236f5d5c47380837a44
Markus Amshove <scm@amshove.org>
2019-05-19 13:41:52 +0200

    Deaktiviere Login ohne Spielernamen

--- android/app/src/main/java/de/hsosnabrueck/bkquiz/activities/LoginActivity.java
+++ android/app/src/main/java/de/hsosnabrueck/bkquiz/activities/LoginActivity.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import de.hsosnabrueck.bkquiz.R;
@@ -22,6 +25,25 @@ public class LoginActivity extends AppCompatActivity {
         if(supportActionBar != null) {
             supportActionBar.hide();
         }
+
+        EditText spielernameText = findViewById(R.id.spielernameText);
+        spielernameText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                 Button loginButton = findViewById(R.id.loginButton);
+                 loginButton.setEnabled(loginButton.length() != 0);
+            }
+        });
     }
 
     public void onLogin(View view) {
