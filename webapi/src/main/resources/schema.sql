c5b49b74c08ae1b18022cc209edca6c62fc041dc
Markus Amshove <scm@amshove.org>
2019-05-30 13:28:49 +0200

    Kein m:n noetig

--- webapi/src/main/resources/schema.sql
+++ webapi/src/main/resources/schema.sql
@@ -1,4 +1,13 @@
-CREATE TABLE IF NOT EXISTS frage (id INT PRIMARY KEY, korrekte_antwort_id INT, frage text);
-CREATE TABLE IF NOT EXISTS frage_antwort(frage_id INT, antwort_id INT, PRIMARY KEY (frage_id, antwort_id));
-CREATE TABLE IF NOT EXISTS antwort (id INT PRIMARY KEY, antwort text);
+CREATE TABLE IF NOT EXISTS antwort
+(
+    id      INT PRIMARY KEY,
+    antwort text,
+    frage_id int
+);
 
+CREATE TABLE IF NOT EXISTS frage
+(
+    id                  INT PRIMARY KEY,
+    korrekte_antwort_id INT,
+    frage               text,
+);
