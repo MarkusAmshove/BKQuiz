6a46b3c57231d08dcb49f0d8265d6fdeb7618d8d
Markus Amshove <scm@amshove.org>
2019-05-30 12:35:48 +0200

    Korrigiere Datenbankverbindung

--- webapi/src/main/resources/schema.sql
+++ webapi/src/main/resources/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE frage (id INT PRIMARY KEY, korrekte_antwort_id INT, frage text);
-CREATE TABLE frage_antwort(frage_id INT, antwort_id INT, PRIMARY KEY (frage_id, antwort_id));
-CREATE TABLE antwort (id INT PRIMARY KEY, antwort text);
+CREATE TABLE IF NOT EXISTS frage (id INT PRIMARY KEY, korrekte_antwort_id INT, frage text);
+CREATE TABLE IF NOT EXISTS frage_antwort(frage_id INT, antwort_id INT, PRIMARY KEY (frage_id, antwort_id));
+CREATE TABLE IF NOT EXISTS antwort (id INT PRIMARY KEY, antwort text);
 
